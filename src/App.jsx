7c53076d865fb4b2f3c11bc9031d492f15b8d9e8
Teo Sayegh <teocoder2021@gmail.com>
2022-05-09 11:07:30 -0300

    ItemCount

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
-// import ItemCount from './components/ItemCount';
+import ItemCount from './components/ItemCount';
+
 
 
 export default function App() {
-
+  
+  const onAdd = (quantity) => {
+    alert(`Agregaste ${quantity} productos`);
+    console.log ("Agregaste " + quantity + " productos");
+  };
   
   return (
       <div className="App">
       <NavBar/>
       <ItemListContainer  greeting={'Hola soy un ItemListContainer'}/>
+      <ItemCount initial={1} stock={5} onAdd={onAdd}/>
       </div>
   );
 }
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,3 +3,49 @@ import { useState } from "react";
 
 import "./styles/ItemCount.css";
 
+export default function ItemCount({stock, initial, onAdd}) {
+    const [quantity, setQuantity] = useState(initial);
+
+    function increase() {
+        if (quantity < stock) {
+            setQuantity(quantity + 1);
+        } 
+    }
+
+    function decrease() {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        } 
+    }
+
+    function addToCart() {
+        onAdd(quantity);
+    }
+
+    return (
+        <div className="main-container">
+        <div className="container-contador">
+            <button
+            className="botones"
+            onClick={decrease}
+            >
+            -
+            </button>
+            <span className="count-container__qty">{quantity}</span>
+            <button
+            className="botones"
+            onClick={increase}
+            >
+            +
+            </button>
+        </div>
+
+        <button
+            className="button-primario"
+            onClick={addToCart}
+        >
+            Añadir
+        </button>
+        </div>
+    );
+}
