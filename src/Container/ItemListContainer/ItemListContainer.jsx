ef3c4546e7cf5322e5d8c8d3d830d83a05d57863
Teo Sayegh <teocoder2021@gmail.com>
2022-05-13 12:50:55 -0300

    Organizacion de carpetas

---
+++ src/Container/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,42 @@
+import {useState, useEffect} from "react"
+import ItemList from "../../components/ItemList/ItemList";
+import "./ItemListContainer.css"
+import { getFetch } from "../../helpers/getFetch"
+import { useParams } from "react-router-dom"
+
+
+export default function ItemListContainer (){
+    const [items,setItems] = useState([]);
+    const [loader,setLoader] = useState(true);
+
+
+    const { id } = useParams() 
+
+    useEffect(() => {
+        if (id) {
+            getFetch()  
+            .then(res=> setItems(res.filter((prods) => prods.category === id)))
+            .catch((err)=> console.log(err))
+            .finally(()=>setLoader(false))                             
+        } else {
+            getFetch()   
+            .then(res=> setItems(res))
+            .catch((err)=> console.log(err))
+            .finally(()=>setLoader(false))                 
+        }
+    }, [id])
+
+    return (
+        <div>
+            {loader ? 
+            <h2 className="centrar-item">Cargando...</h2>
+            : 
+            <ItemList items={items} />}
+        </div>
+    );
+}
+
+
+
+
+
