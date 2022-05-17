e0bf36f65b952055b81360997a6001b314e7ebdd
Teo Sayegh <teocoder2021@gmail.com>
2022-05-17 12:47:28 -0300

    Optimizando diseño de ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Card from "react-bootstrap/Card";
 import ItemCount from "../ItemCount/ItemCount"
+import "./ItemDetail.css"
 
 
 export default function ItemDetail ({item}) {
     return (
         <Card className="bg-dark text-white">
-        <Card.Img src={item.Img} alt="Card image" />
+        <Card.Img className="dimension" src={item.img} alt="Imagen del Producto" />
         <Card.ImgOverlay>
             <Card.Title>{item.name}</Card.Title>
             <Card.Text>
             {`Precio: $${item.price}`}
             </Card.Text>
         </Card.ImgOverlay>
-        <ItemCount initial={1} id={item.id} stock={5} onAdd={(quantity)=> console.log(`${quantity} unidad/es de ${item.name} agregada/s al pedido`)}/>
+        <ItemCount initial={1} stock={5} onAdd={(quantity)=> console.log(`${quantity} unidad/es de ${item.name} agregada/s al pedido`)}/>
         </Card>
     )
 }
